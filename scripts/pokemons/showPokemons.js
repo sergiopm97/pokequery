d0eac59199a68b02ceed98863a2551fac453c3e7
Sergio Peña Muñoz <sergio.pm.97@protonmail.com>
2022-01-24 09:19:00 +0100

    Give aesthetics to Pokémon cards
    Placing the ID and type of the Pokémon in the corners of the card - Implementing a hover effect at the edges of the cards

--- scripts/pokemons/showPokemons.js
+++ scripts/pokemons/showPokemons.js
@@ -12,6 +12,9 @@ const showPokemons = (pokemons) => {
         let pokemonCard = document.createElement("div");
         pokemonCard.className = "pokemon-card";
 
+        let pokemonContent = document.createElement("div");
+        pokemonContent.className = "pokemon-content"
+
         let pokemonId = document.createElement("h1");
         pokemonId.className = "pokemon-id";
 
@@ -21,16 +24,25 @@ const showPokemons = (pokemons) => {
         let pokemonName = document.createElement("h1");
         pokemonName.className = "pokemon-name";
 
+        let firstPokemonType = document.createElement("h3");
+        firstPokemonType.className = "pokemon-type--1";
+
         pokemon.then(data => {
             pokemonId.textContent = data.id;
+            pokemonId.id = data.types[0].type.name + "--id";
             pokemonImage.src = data.sprites.other.dream_world.front_default;
             pokemonName.textContent = data.name.charAt(0).toUpperCase() + data.name.slice(1);
-            pokemonCard.id = data.types[0].type.name;
+            firstPokemonType.textContent = data.types[0].type.name.charAt(0).toUpperCase() + data.types[0].type.name.slice(1);
+            firstPokemonType.id = data.types[0].type.name + "--type";
+            pokemonCard.id = data.types[0].type.name + "--card";
         })
 
+        pokemonContent.appendChild(pokemonImage);
+        pokemonContent.appendChild(pokemonName);
+
         pokemonCard.appendChild(pokemonId);
-        pokemonCard.appendChild(pokemonImage);
-        pokemonCard.appendChild(pokemonName);
+        pokemonCard.appendChild(firstPokemonType);
+        pokemonCard.appendChild(pokemonContent);
 
         pokemonsContainer.appendChild(pokemonCard);
     }
